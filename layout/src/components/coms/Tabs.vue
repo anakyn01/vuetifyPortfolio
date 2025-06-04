d7e21b5c292dd10b46b2998c27eddb6d4eafc0cf
Your Name <you@example.com>
2025-06-04 17:01:28 +0900

    77

--- layout/src/components/coms/Tabs.vue
+++ layout/src/components/coms/Tabs.vue
@@ -25,7 +25,7 @@
     </v-card>
 
 </v-col>  
-<v-col cols="12" md="3">
+<v-col cols="12" md="9">
 <v-card>
     <v-tabs v-model="tab" align-tabs="center" color="deep-purple-accent-4">
         <v-tab :value="1">Landscape</v-tab>
@@ -33,15 +33,24 @@
         <v-tab :value="3">Abstract</v-tab>
     </v-tabs>
     <v-tabs-window v-model="tab">
-        <v-tabs-window-item>
-            
+        <v-tabs-window-item v-for="n in 3" :key="n" :value="n">
+<v-container fluid>
+    <v-row>
+        <v-col
+        v-for="i in 6" :key="i" cols="12" md="4">
+            <v-img
+            :lazy-src="`https://picsum.photos/10/6?image=${i * n * 5 + 10}`"
+            :src="`https://picsum.photos/500/300?image=${i * n * 5 + 10}`"
+            height="205"
+            cover/>
+        </v-col>
+    </v-row>
+</v-container>           
         </v-tabs-window-item>
     </v-tabs-window>
 </v-card>
 </v-col>
-<v-col cols="12" md="3">
-
-</v-col>           
+         
         </v-row>
     </v-container>
 </template>
