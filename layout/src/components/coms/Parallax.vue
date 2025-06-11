86d40ecdcf4868b617a517e178cd4e35781ce135
Your Name <you@example.com>
2025-06-11 17:48:46 +0900

    8

--- layout/src/components/coms/Parallax.vue
+++ layout/src/components/coms/Parallax.vue
@@ -1,15 +1,15 @@
 <template>
-    <v-container>
-        <v-row>
-            <v-col cols="12" md="4">
-
-            </v-col>
-            <v-col cols="12" md="4">
-                
-            </v-col>
-            <v-col cols="12" md="4">
-                
-            </v-col>
-        </v-row>
-    </v-container>
+<v-parallax src="http://cdn.vuetifyjs.com/images/parallax/material.jpg">
+</v-parallax>
+<h1>글씨가 들어간</h1>
+<v-parallax src="http://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg">
+    <div class="d-flex flex-column fill-height justify-center align-center text-white">
+        <h1 class="text-h4 font-weight-thin mb-4">
+            엌ㅋㅋㅋㅋ
+        </h1>
+        <h4 class="subheading">
+            한시간 남..
+        </h4>
+    </div>
+</v-parallax>
 </template>
